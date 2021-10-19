1ff156ff3cd41d70cb049b1b2d341ada442ea55c
htoo123456 <90297565+htoo123456@users.noreply.github.com>
2021-10-19 10:21:36 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const tagsEl = document.querySelector(".tags");
+const textarea = document.querySelector("textarea");
+
+textarea.addEventListener("keyup", (e) => {
+    createElement(e.target.value);
+
+    if (e.key == "Enter") {
+        e.target.value = "";
+        randomSelect();
+    }
+})
+
+const createElement = (input) => {
+    const arr = input.split(",").filter(tag => tag.trim() !== "").map(tag => tag.trim());
+    tagsEl.innerHTML = "";
+    arr.forEach(tag => {
+        const span = document.createElement("span");
+        span.classList.add("tag")
+        span.append(tag);
+        tagsEl.append(span);
+    });
+}
+
+const randomSelect = () => {
+    const times = 30;
+    const inerval = setInterval(()=> {
+        const tag = pickRandomSelect();
+        highLightTag(tag);
+        setTimeout(() => {
+            unHighLightTag(tag);
+        },100);
+    }, 100);
+    setTimeout(()=> {
+        clearInterval(inerval);
+        setTimeout(()=> {
+            const randomTag = pickRandomSelect();
+        highLightTag(randomTag);
+        }, 100);
+        
+    },times * 100)
+}
+
+const pickRandomSelect = () => {
+    const tags = document.querySelectorAll(".tag");
+    return tags[Math.floor(Math.random() * tags.length)];
+}
+
+const highLightTag = (tag) => {
+    tag.classList.add("highLight");
+}
+
+const unHighLightTag = (tag) => {
+    tag.classList.remove("highLight");
+}
